41e52ba389fe9a8f65133c33807b9c79ce501a1f
Chris Jenkinson <chris@chrisjenkinson.org>
2011-03-31 20:38:42 +0100

    New lines bugfix.

--- modules/sfMaraeCategory/templates/showSuccess.xml.php
+++ modules/sfMaraeCategory/templates/showSuccess.xml.php
@@ -1,4 +1,6 @@
-<?php echo "<" . "?" ?>xml version="1.0"?> 
+<?php use_helper('XssSafe') ?>
+<?php $postsReversedPurified = $sf_data->get('postsReversed', ESC_XSSSAFE) ?>
+<?php echo "<" . "?" ?>xml version="1.0"?>
 <rss version="2.0">
 	<channel>
 		<title><?php printf('Forum: %s', $root['name']) ?></title>
@@ -11,11 +13,12 @@
 		<docs>http://www.rssboard.org/rss-specification</docs>
 		<generator>sfMaraePlugin</generator>
 
-<?php foreach ($postsReversed as $item): ?>
+<?php foreach ($postsReversed as $k => $item): ?>
+
 		<item>
 			<title><?php echo $item['title'] ?></title>
 			<link><?php echo url_for('@post_show_post?id=' . $item['id'] . '&replies=' . (($item['rgt'] - $item['lft'] - 1)/2), true) ?></link>
-			<description><?php echo $item['message'] ?></description>
+			<description><![CDATA[<?php echo $postsReversedPurified[$k]['message'] ?>]]></description>
 			<pubDate><?php echo date('r', strtotime($item['created_at'])) ?></pubDate>
 			<guid isPermaLink="true"><?php echo url_for('@post_show?id=' . $item['id'], true) ?></guid>
 		</item>
